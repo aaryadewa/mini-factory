abc1dd107daabe349ac54a0700b3554acd6576f8
Ananta Aryadewa <ananta.aryadewa@gmail.com>
2020-08-14 22:59:06 +0700

    3 Add basic input output

--- src/main/java/com/aaryadewa/dev/App.java
+++ src/main/java/com/aaryadewa/dev/App.java
@@ -1,5 +1,7 @@
 package com.aaryadewa.dev;
 
+import java.util.Scanner;
+
 import com.aaryadewa.dev.util.Logger;
 
 /**
@@ -7,22 +9,67 @@ import com.aaryadewa.dev.util.Logger;
  */
 public final class App {
     private static final Logger logger = new Logger(App.class);
+    private boolean running = false;
+    private final String[] menu;
 
     private App() {
         logger.info("*** Mini Factory ***");
+        menu = new String[] {
+            "(1) Adjust output verbosity",
+            "(2) Exit"
+        };
     }
 
     public void start() {
         logger.info("Starting up the application...");
+        running = true;
     }
 
     public void stop() {
         logger.info("Stopping the application...");
     }
 
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public void showMenu() {
+        logger.prompt("\nPlease select a menu:");
+        for (String item : menu) {
+            logger.prompt(item);
+        }
+    }
+
     public static void main(String[] args) {
         App app = new App();
+        Scanner input = new Scanner(System.in);
+
         app.start();
+
+        while (app.isRunning()) {
+            app.showMenu();
+            logger.debug("Waiting for user input...");
+            long selection = input.nextLong();
+
+            if (selection == 1) {
+                logger.showLevels();
+                Logger.setLevel(input.nextInt());
+            }
+
+            else if (selection == 2) {
+                app.setRunning(false);
+            }
+
+            else {
+                logger.error("There is no such menu (" + selection + ")");
+            }
+        }
+
+        input.close();
         app.stop();
     }
 }
--- src/main/java/com/aaryadewa/dev/util/Logger.java
+++ src/main/java/com/aaryadewa/dev/util/Logger.java
@@ -3,11 +3,64 @@ package com.aaryadewa.dev.util;
 public class Logger {
   private final Class<? extends Object> clazz;
 
+  private static final int DEBUG = 4;
+  private static final int INFO = 3;
+  private static final int WARN = 2;
+  private static final int ERROR = 1;
+
+  private static final String[] LEVEL_OPTIONS = new String[] {
+    "(" + DEBUG + ") Show all messages",
+    "(" + INFO + ") Don't show debug messages",
+    "(" + WARN + ") Show warning and error messages only",
+    "(" + ERROR + ") I don't care all but error messages"
+  };
+
+  private static int level = DEBUG;
+
   public Logger(Class<? extends Object> clazz) {
     this.clazz = clazz;
   }
 
+  public void debug(String message) {
+    if (level >= DEBUG)
+      println("[DEBUG] " + clazz.getName() + " - " + message);
+  }
+
   public void info(String message) {
-    System.out.println(clazz + " - " + message);
+    if (level >= INFO)
+      println("[INFO] " + clazz.getName() + " - " + message);
+  }
+
+  public void warn(String message) {
+    if (level >= WARN)
+      println("[WARN] " + clazz.getName() + " - " + message);
+  }
+
+  public void error(String message) {
+    if (level >= ERROR)
+      println("[ERROR] " + clazz.getName() + " - " + message);
+  }
+
+  public void prompt(String message) {
+    println(message);
+  }
+
+  public void showLevels() {
+    for (String option : LEVEL_OPTIONS) {
+      prompt(option);
+    }
+  }
+
+  public static void setLevel(int level) {
+    if (level > DEBUG) {
+      level = DEBUG;
+    } else if (level < ERROR) {
+      level = ERROR;
+    }
+    Logger.level = level;
+  }
+
+  private void println(String message) {
+    System.out.println(message);
   }
 }
